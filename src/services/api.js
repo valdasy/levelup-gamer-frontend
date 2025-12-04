b079cbbd67984a70b320b4489a201511759dd4c1
alejandro <alejandrobricenobenavides@gmail.com>
2025-12-04 14:01:17 -0300

    Tercer cambio para el ramo de java

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Usamos la variable de entorno, o el localhost por defecto si no existe
-const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8081/api";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8080/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
